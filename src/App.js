e151b50dc64d7c8d80d17559866c718c2858c0b1
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-16 11:48:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,40 @@ import './App.css';
 import Micomponente from './componentes/Micomponente';
 
 
-const container=document.getElementById('root')
+function HolaMundo(nombre,edad){
+  var presentacion=<div>
+    <h2>Hola, soy {nombre}</h2>
+    <h2>Tengo {edad} años</h2>
+  </div>
+  return presentacion;
+ 
+}
+function App() {
+  var nombre ="Rocibel Alvarado";
 
-ReactDOM.render(<Micomponente/>,container)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Hola Rocibel</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {
+        HolaMundo(nombre,26)
+        }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <section className="Componentes">
+        <Micomponente/>
+      </section>
+      </header>
+    </div>
+  );
+}
